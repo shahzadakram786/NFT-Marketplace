d3db114ead5a8daba8b8b21728150d20f63f4968
shahzadakram786 <alyakram786@gmail.com>
2025-02-01 00:48:19 +0500

    Feed: working on the stylings

--- src/components/discoverMoreNft/ReDiscoverNft.jsx
+++ src/components/discoverMoreNft/ReDiscoverNft.jsx
@@ -1,50 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import astroImag from "../../assets/bgAstro.png";
-import landsImag from "../../assets/bglandscape.png";
-import bgSpaceship from "../../assets/main1spaceship.png";
-import person5 from "../../assets/person5.png";
-import person3 from "../../assets/person3.png";
-import person11 from "../../assets/person11.png";
+
 import NftCards from "../nftCards/NftCards";
 
-const ReDiscoverNft = () => {
-  const Discover = [
-    {
-      id: 1,
-      mainImage: bgSpaceship,
-      title: "Distant Galaxy",
-      dp: person3,
-      pname: "MoonDancer",
-      price: 1.63,
-      curr: "ETH",
-      highestBid: 0.33,
-    },
-    {
-      id: 2,
-      mainImage: landsImag,
-      title: "Life On Edena",
-      dp: person5,
-      pname: "NebulaKid",
-      price: 1.63,
-      curr: "ETH",
-      highestBid: 0.33,
-    },
-    {
-      id: 3,
-      mainImage: astroImag,
-      title: "AstroFiction",
-      dp: person11,
-      pname: "Spaceone",
-      price: 1.63,
-      curr: "ETH",
-      highestBid: 0.33,
-    },
-  ];
+const ReDiscoverNft = ({data}) => {
+  
 
   return (
     <div className="grid grid-cols-3 max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-2 max-sm:w-full  gap-8 ">
-      {Discover.map((item) => (
+      {data.map((item) => (
        <NftCards
        key={item.id}
        mainImage={item.mainImage}
@@ -55,7 +19,7 @@ const ReDiscoverNft = () => {
        curr={item.curr}
        highestBid={item.highestBid}
        link="/marketplace"
-       />
+        />
       ))}
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,34 @@
-import Catogries from '../../components/catogries'
+import Catogries from "../../components/catogries";
 import React from "react";
 import Hero from "../../components/hero/Hero";
 import TrendingCollection from "../../components/TrendingCollection/TrendingCollection";
 import Topcreators from "../../components/topCreators/Topcreators";
 import DiscoverNft from "../../components/discoverMoreNft/DiscoverNft";
-import WorksComponent from '../../components/works';
-import DigestComponent from '../../components/digest';
-import Trending from '../../components/TrendingCollection/TrendingCollectionData';
+import WorksComponent from "../../components/works";
+import DigestComponent from "../../components/digest";
+import Trending from "../../components/TrendingCollection/TrendingCollectionData";
+import topCreators from "../../components/topCreators/TopCardsData";
+import categories from "../../components/catogries/catogriesData";
+import Discover from "../../components/discoverMoreNft/discoverData";
 
 const Home = () => {
-
   return (
     <>
-    
-
-
-
-    
-      
+      <section className="bg-[#2B2B2B] flex flex-col justify-center items-center gap-16 py-20">
         <Hero />
-    
-      <section className="bg-[#2B2B2B] flex justify-center items-center py-20">
-          <TrendingCollection data={Trending}/>
-      </section>
-      <section className="py-20">
-          <Topcreators/>
-      </section>
-      <section className="bg-[#2B2B2B] flex justify-center items-center py-20">
-          <Catogries/>
-      </section>
-      <section className="bg-[#2B2B2B] flex justify-center items-center py-20">
-          <DiscoverNft/>
-      </section>
-      <section className="bg-[#2B2B2B] flex justify-center items-center py-20">
-          <WorksComponent/>
-      </section>
-      <section className="bg-[#2B2B2B] flex justify-center items-center py-20">
-          <DigestComponent/>
-      </section>
 
+        <TrendingCollection data={Trending} />
+
+        <Topcreators data={topCreators} />
+
+        <Catogries data={categories} />
+
+        <DiscoverNft data={Discover} />
+
+        <WorksComponent />
+
+        <DigestComponent />
+      </section>
     </>
   );
 };
