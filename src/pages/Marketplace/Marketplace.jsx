086053f4906f3b38621f18b020a86a76c3c64450
shahzadakram786 <alyakram786@gmail.com>
2025-02-10 18:09:09 +0500

    Feed: working on the marketplace

--- src/pages/Marketplace/Marketplace.jsx
+++ src/pages/Marketplace/Marketplace.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+import Wrapper from "../../components/wrapper/Wrapper";
 
 const Marketplace = () => {
   return (
-    <div className="min-h-[calc(100vh-530px)] bg-gray-800 text-white flex justify-center items-center">
+    <Wrapper >
+      <div className="w-full flex flex-col justify-center items-center" >
 
-    <h1 className='text-xl '>
-          comming soon - - - 
-    </h1>
-  </div>
-  )
-}
+      <div className="flex flex-col gap-6 p-2 w-[60%]">
+        <div className="p-2 flex flex-col gap-3">
+          <h1 className="text-5xl text-white font-bold">Browse Markeetplace</h1>
+          <p className="text-xl text-white">
+            Browse through more than 50k NFTs on the NFT Marketplace
+          </p>
+        </div>
+        {/* //search abr  */}
+        <div>
+          <div className="flex gap-1 items-center justify-between px-3 text-white rounded-2xl border-gray-600 border outline-none">
+            <input
+              type="text"
+              placeholder="Search your favourite NFTs"
+              className="bg-transparent p-2 w-full outline-none text-[#616160] text-xl"
+            />
+            <BiSearch size={25} color="white" />
 
-export default Marketplace
+            {/* logo   */}
+          </div>
+        </div>
+      </div>
+        
+      <hr className="mt-20 w-full h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+
+    <div className='flex flex-col justify-center items-center w-[60%]'>
+
+      <div className="flex gap-4 text-white text-2xl w-full">
+
+      
+                <button className="p-5 px-10 flex gap-3 items w-[50%] justify-center" >NFTs
+                   <h1 className="bg-gray-500 rounded-full py-1 px-2 text-sm">333</h1>
+                </button>
+          
+           
+                <button className="p-5 px-10 flex gap-3 items justify-center  w-[50%]" >Collections
+                   <h1 className="bg-gray-500 rounded-full py-1 px-2 text-sm">346</h1>
+                </button>
+           
+      </div>
+    </div>
+      
+
+      </div>
+      
+    </Wrapper>
+  );
+};
+
+export default Marketplace;
