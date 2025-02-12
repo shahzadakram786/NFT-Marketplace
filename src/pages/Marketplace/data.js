077cb6fcb71e6f4f8bbbac5e8465e6f124a3486b
shahzadakram786 <alyakram786@gmail.com>
2025-02-12 17:58:12 +0500

    Feed: working on maketplace nft cards components

---
+++ src/pages/Marketplace/data.js
@@ -0,0 +1,153 @@
+import magicMushroom from "../../assets/marketplace/magicMushroom.png";
+import happyRobot1 from "../../assets/marketplace/happyRobot1.png";
+import happyRobot2 from "../../assets/marketplace/happyRobot2.png";
+import designerBear from "../../assets/marketplace/designerBear.png";
+import colorfulDog1 from "../../assets/marketplace/colorfulDog1.png";
+import dancingRobot from "../../assets/marketplace/dancingRobot.png";
+import cherryGirl from "../../assets/marketplace/cherryGirl.png";
+import spaceTravel from "../../assets/marketplace/spaceTravel.png";
+import sunsetDim from "../../assets/marketplace/sunsetDim.png";
+import desertWalk from "../../assets/marketplace/desertWalk.png";
+import iceCreamApe from "../../assets/marketplace/iceCreamApe.png";
+import colorfulDog2 from "../../assets/marketplace/colorfulDog2.png";
+import avatar1 from "../../assets/marketplace/avatar1.png";
+import avatar2 from "../../assets/marketplace/avatar2.png";
+import avatar3 from "../../assets/marketplace/avatar3.png";
+import avatar4 from "../../assets/marketplace/avatar4.png";
+
+const DataMarketPlace = [
+  {
+    id: 1,
+    mainImage: magicMushroom,
+    title: "Magic Mushroom 0325",
+    dp: avatar1,
+    pname: "Smookie",
+    price: 1.85,
+    curr: "ETH",
+    highestBid: 0.35,
+    category: "NFT",
+  },
+  {
+    id: 2,
+    mainImage: happyRobot1,
+    title: "Happy Robot 032",
+    dp: avatar2,
+    pname: "2kXRoboArts",
+    price: 1.63,
+    curr: "ETH",
+    highestBid: 0.33,
+    category: "NFT",
+  },
+  {
+    id: 3,
+    mainImage: happyRobot2,
+    title: "Happy Robot 024",
+    dp: avatar3,
+    pname: "2kXRoboArts",
+    price: 1.85,
+    curr: "ETH",
+    highestBid: 0.35,
+    category: "NFT",
+  },
+  {
+    id: 4,
+    mainImage: designerBear,
+    title: "Designer Bear",
+    dp: avatar4,
+    pname: "Mr. Bear",
+    price: 1.95,
+    curr: "ETH",
+    highestBid: 0.37,
+    category: "NFT",
+  },
+  {
+    id: 5,
+    mainImage: colorfulDog1,
+    title: "Colorful Dog 0326",
+    dp: avatar1,
+    pname: "DogLoverX",
+    price: 1.63,
+    curr: "ETH",
+    highestBid: 0.33,
+    category: "NFT",
+  },
+  {
+    id: 6,
+    mainImage: dancingRobot,
+    title: "Dancing Robot 0312",
+    dp: avatar2,
+    pname: "RoboLicious",
+    price: 1.85,
+    curr: "ETH",
+    highestBid: 0.35,
+    category: "NFT",
+  },
+  {
+    id: 7,
+    mainImage: cherryGirl,
+    title: "Cherry Blossom Girl 035",
+    dp: avatar3,
+    pname: "NeoShade",
+    price: 1.73,
+    curr: "ETH",
+    highestBid: 0.34,
+    category: "NFT",
+  },
+  {
+    id: 8,
+    mainImage: spaceTravel,
+    title: "Space Travel",
+    dp: avatar4,
+    pname: "NebulaKid",
+    price: 1.65,
+    curr: "ETH",
+    highestBid: 0.32,
+    category: "NFT",
+  },
+  {
+    id: 9,
+    mainImage: sunsetDim,
+    title: "Sunset Dimension",
+    dp: avatar1,
+    pname: "ArtSeeker",
+    price: 1.93,
+    curr: "ETH",
+    highestBid: 0.36,
+    category: "NFT",
+  },
+  {
+    id: 10,
+    mainImage: desertWalk,
+    title: "Desert Walk",
+    dp: avatar2,
+    pname: "EarthGo",
+    price: 1.75,
+    curr: "ETH",
+    highestBid: 0.34,
+    category: "NFT",
+  },
+  {
+    id: 11,
+    mainImage: iceCreamApe,
+    title: "IceCream Ape 0324",
+    dp: avatar3,
+    pname: "Ape Crew",
+    price: 1.89,
+    curr: "ETH",
+    highestBid: 0.35,
+    category: "NFT",
+  },
+  {
+    id: 12,
+    mainImage: colorfulDog2,
+    title: "Colorful Dog 0344",
+    dp: avatar4,
+    pname: "PawGraphy",
+    price: 1.63,
+    curr: "ETH",
+    highestBid: 0.33,
+    category: "NFT",
+  },
+];
+
+export default DataMarketPlace;
