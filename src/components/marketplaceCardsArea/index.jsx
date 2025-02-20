6bda35a381bac3a4ebbbec78e691be994da49957
shahzadakram786 <alyakram786@gmail.com>
2025-02-20 19:33:11 +0500

    Feed: completing the makeetplace page

--- src/components/marketplaceCardsArea/index.jsx
+++ src/components/marketplaceCardsArea/index.jsx
@@ -21,13 +21,13 @@ const handleChangeCategory =(category)=>{
           <hr className=" w-[100%] h-px border-t-0 bg-transparent bg-gradient-to-r  from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
 
             <div className="flex flex-col justify-center items-center">
-            <div className="flex gap-4 text-gray-500 text-2xl w-full">
+            <div className="flex gap-4 text-gray-500 text-2xl w-full max-sm:text-[16px]">
               <button onClick={()=>handleChangeCategory('NFT')}
                 value="Nft"
                 className="p-5  px-10 flex gap-3 items w-[50%] justify-center  focus:text-white focus:border-b-2 border-gray-500"
               >
                 NFTs
-                <h1 className="bg-gray-500 text-white items-center flex rounded-full py-1 px-2 text-sm">
+                <h1 className="bg-gray-500 text-white items-center flex rounded-full py-1 px-2 text-sm max-sm:hidden">
                   333
                 </h1>
               </button>
@@ -37,7 +37,7 @@ const handleChangeCategory =(category)=>{
                 className="p-5 px-10 flex gap-3 items justify-center  w-[50%] focus:text-white focus:border-b-2 border-gray-500"
               >
                 Collections
-                <h1 className="bg-gray-500 text-white flex items-center rounded-full py-1 px-2 text-sm">
+                <h1 className="bg-gray-500 text-white flex items-center rounded-full py-1 px-2 text-sm max-sm:hidden">
                   346
                 </h1>
               </button>
